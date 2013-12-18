a8e5ed9ef25f67efefc9822d16c3109ca7d1e73c
jplouis <jplouis@gmail.com>
2013-12-18 13:21:14 -0600

    remove duplicate arg

--- metric-consumer-app/src/main/bin/metric-consumer-app.sh
+++ metric-consumer-app/src/main/bin/metric-consumer-app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JVM_ARGS="$JVM_ARGS -XX:+HeapDumpOnOutOfMemoryError"
+JVM_ARGS="$JVM_ARGS"
 JVM_XMX="-Xmx1024m"
 
 exec java -server -XX:+HeapDumpOnOutOfMemoryError ${JVM_XMX} ${JVM_ARGS}  -jar lib/${project.artifactId}/${project.artifactId}-${project.version}.jar server etc/${project.artifactId}/configuration.yaml
