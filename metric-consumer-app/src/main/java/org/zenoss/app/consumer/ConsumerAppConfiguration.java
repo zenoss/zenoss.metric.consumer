5500d37c300c4708ea315e30aac98f7886180a3f
Seth Cleveland <seth.cleveland@gmail.com>
2014-02-12 14:17:54 -0600

    configuration option to inject metric tags from http parameters

--- metric-consumer-app/src/main/java/org/zenoss/app/consumer/ConsumerAppConfiguration.java
+++ metric-consumer-app/src/main/java/org/zenoss/app/consumer/ConsumerAppConfiguration.java
@@ -12,11 +12,14 @@
 package org.zenoss.app.consumer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.api.client.util.Lists;
 import org.zenoss.app.AppConfiguration;
 import org.zenoss.app.consumer.metric.MetricServiceConfiguration;
 import org.zenoss.app.metric.zapp.MetricReporterConfig;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConsumerAppConfiguration extends AppConfiguration {
 
@@ -27,6 +30,10 @@ public class ConsumerAppConfiguration extends AppConfiguration {
     @Valid
     @JsonProperty("metricReporter")
     private MetricReporterConfig metricReporterConfig = new MetricReporterConfig.Builder().setReportFrequencySeconds(30).build();
+
+    @Valid
+    @JsonProperty("httpParameterTags")
+    private List<String> httpParameterTags = Lists.newArrayList();
 
     /**
      * Configuration details for the metric service
@@ -44,5 +51,23 @@ public class ConsumerAppConfiguration extends AppConfiguration {
      */
     public MetricReporterConfig getMetricReporterConfig() {
         return metricReporterConfig;
+    }
+
+
+    /**
+     * A list of http query parameters to include with each metric. Each element is matched as a prefix against
+     * parameter in the http servlet request. If multiple values are associated with a parameter, the first one
+     * is used.
+     */
+    public List<String> getHttpParameterTags() {
+        return httpParameterTags;
+    }
+
+    public void setHttpParameterTags(List<String> httpParameterTags) {
+        this.httpParameterTags = httpParameterTags;
+    }
+
+    public void setMetricServiceConfiguration(MetricServiceConfiguration metricServiceConfiguration) {
+        this.metricServiceConfiguration = metricServiceConfiguration;
     }
 }
--- metric-consumer-app/src/test/java/org/zenoss/app/consumer/metric/MetricWebResourceTest.java
+++ metric-consumer-app/src/test/java/org/zenoss/app/consumer/metric/MetricWebResourceTest.java
@@ -5,27 +5,38 @@ import com.google.common.collect.Lists;
 import com.sun.jersey.api.client.WebResource;
 import com.yammer.dropwizard.testing.ResourceTest;
 import com.yammer.dropwizard.validation.InvalidEntityException;
+import org.junit.Before;
 import org.junit.Test;
+import org.zenoss.app.consumer.ConsumerAppConfiguration;
 import org.zenoss.app.consumer.metric.data.Control;
 import org.zenoss.app.consumer.metric.data.Metric;
 import org.zenoss.app.consumer.metric.data.MetricCollection;
 import org.zenoss.app.consumer.metric.remote.MetricWebResource;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-public class MetricWebResourceTest extends ResourceTest {
+public class MetricWebResourceTest {
 
     final Control control = new Control();
     final MetricService service = mock(MetricService.class);
+    final ConsumerAppConfiguration configuration = new ConsumerAppConfiguration();
+    HttpServletRequest request;
+    MetricWebResource resource;
 
-    @Override
-    protected void setUpResources() throws Exception {
+    @Before
+    public void setUp() throws Exception {
+        List<String> parameters = Lists.newArrayList();
+        configuration.setHttpParameterTags( parameters);
         when(service.push(anyListOf(Metric.class))).thenReturn(control);
-        addResource(new MetricWebResource(service));
+        request = mock(HttpServletRequest.class);
+        resource = new MetricWebResource(service, configuration);
     }
 
     @Test
@@ -35,43 +46,33 @@ public class MetricWebResourceTest extends ResourceTest {
         Metric metric = new Metric("name", 0, 1.0, tags);
         MetricCollection mc = new MetricCollection();
         mc.setMetrics(Lists.newArrayList(metric));
-        WebResource resource = client().resource("/api/metrics/store");
-        WebResource.Builder builder = resource.type(MediaType.APPLICATION_JSON_TYPE);
-        assertThat(builder.post(Control.class, mc)).isEqualTo(control);
+
+        assertThat(resource.post(mc, request)).isEqualTo(control);
         verify(service).push(mc.getMetrics());
     }
 
     @Test
     public void testPostMessageInjectsTags() throws Exception {
+        configuration.getHttpParameterTags().add("controlplane");
         Map<String, String> tags = Maps.newHashMap();
         tags.put("blam", "blamo");
         Metric metric = new Metric("name", 0, 1.0, tags);
         MetricCollection mc = new MetricCollection();
         mc.setMetrics(Lists.newArrayList(metric));
 
-        WebResource resource = client().resource("/api/metrics/store").queryParam("tenantId", "1").queryParam("serviceId", "2");
-        WebResource.Builder builder = resource.type(MediaType.APPLICATION_JSON_TYPE);
-        assertThat(builder.post(Control.class, mc)).isEqualTo(control);
+        List<String> parameters = Lists.newArrayList();
+        parameters.add("controlplane_tenant_id");
+        parameters.add("controlplane_service_id");
+        when(request.getParameterNames()).thenReturn(Collections.enumeration(parameters));
+        when(request.getParameter("controlplane_tenant_id")).thenReturn("1");
+        when(request.getParameter("controlplane_service_id")).thenReturn("2");
+        assertThat(resource.post(mc, request)).isEqualTo(control);
 
         tags = Maps.newHashMap();
         tags.put("blam", "blamo");
-        tags.put("tenantId", "1");
-        tags.put("serviceId", "2");
+        tags.put("controlplane_tenant_id", "1");
+        tags.put("controlplane_service_id", "2");
         Metric expected_metric = new Metric("name", 0, 1.0, tags);
         verify(service).push( Lists.newArrayList( expected_metric));
-    }
-
-    @Test(expected = InvalidEntityException.class)
-    public void testPostNullMessage() throws Exception {
-        WebResource resource = client().resource("/api/metrics/store");
-        WebResource.Builder builder = resource.type(MediaType.APPLICATION_JSON_TYPE);
-        builder.post(Control.class, null);
-    }
-
-    @Test(expected = InvalidEntityException.class)
-    public void testPostEmptyMessage() throws Exception {
-        WebResource resource = client().resource("/api/metrics/store");
-        WebResource.Builder builder = resource.type(MediaType.APPLICATION_JSON_TYPE);
-        builder.post(Control.class, new MetricCollection());
     }
 }
