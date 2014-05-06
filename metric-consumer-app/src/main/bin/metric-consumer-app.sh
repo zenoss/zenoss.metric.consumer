618670f86aaff86b20949442f91d0eac0ee33184
Ian McCracken <ian.mccracken@gmail.com>
2014-05-06 09:03:59 -0500

    Modify script to be intelligent about jar vs. src

--- metric-consumer-app/src/main/bin/metric-consumer-app.sh
+++ metric-consumer-app/src/main/bin/metric-consumer-app.sh
@@ -3,4 +3,11 @@
 JVM_ARGS="$JVM_ARGS"
 JVM_XMX="-Xmx1024m"
 
-exec java -server -XX:+HeapDumpOnOutOfMemoryError ${JVM_XMX} ${JVM_ARGS}  -jar lib/${project.artifactId}/${project.artifactId}-${project.version}.jar server etc/${project.artifactId}/configuration.yaml
+LIBDIR=${ZENHOME}/lib/${project.artifactId}
+ZAPP_JAR=${LIBDIR}/${project.artifactId}-${project.version}.jar
+
+if [ -f ${ZAPP_JAR} ]; then
+    exec java -server -XX:+HeapDumpOnOutOfMemoryError ${JVM_XMX} ${JVM_ARGS}  -jar ${ZAPP_JAR} server ${ZENHOME}/etc/${project.artifactId}/configuration.yaml
+else
+    cd ${LIBDIR} &&  mvn -DskipTests=true compile exec:java
+fi
