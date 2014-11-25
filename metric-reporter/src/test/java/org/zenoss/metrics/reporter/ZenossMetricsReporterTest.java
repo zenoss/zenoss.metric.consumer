0dd9820a9b987d163a5ac0d9271ee077893d2b6f
John Crocker <jcrocker@zenoss.com>
2014-11-25 15:20:35 -0600

    Insert copyright/license boilerplate
    find -name \*.java | xargs -I% bash -c 'echo "$(cat header.txt %)" > %'

--- metric-reporter/src/test/java/org/zenoss/metrics/reporter/ZenossMetricsReporterTest.java
+++ metric-reporter/src/test/java/org/zenoss/metrics/reporter/ZenossMetricsReporterTest.java
@@ -1,3 +1,13 @@
+/*
+ * ****************************************************************************
+ *
+ *  Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+ *
+ *  This content is made available according to terms specified in
+ *  License.zenoss distributed with this file.
+ *
+ * ***************************************************************************
+ */
 package org.zenoss.metrics.reporter;
 
 import com.google.common.collect.ImmutableMap;
