225fceabe6dcb6e09ce9ced04fc7c3cc2978a080
Jason Peacock <jpeacock@zenoss.com>
2020-09-09 12:26:01 -0500

    Add jvm arguments to support Java 11.
    Fixes ZEN-32964.

--- metric-consumer-app/src/main/bin/metric-consumer-app.sh
+++ metric-consumer-app/src/main/bin/metric-consumer-app.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-JVM_ARGS="$JVM_ARGS"
+JVM_ARGS="$JVM_ARGS --add-opens java.base/jdk.internal.misc=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
 JVM_XMX="-Xmx1024m"
 
 LIBDIR=/opt/zenoss/lib/${project.artifactId}
 ZAPP_JAR=${LIBDIR}/${project.artifactId}-${project.version}.jar
 
 if [ -f ${ZAPP_JAR} ]; then
-    exec java -server -XX:OnOutOfMemoryError="kill -9 %p" ${JVM_XMX} ${JVM_ARGS}  -jar ${ZAPP_JAR} server etc/${project.artifactId}/configuration.yaml
+    exec java -server -XX:OnOutOfMemoryError="kill -9 %p" ${JVM_XMX} ${JVM_ARGS} -jar ${ZAPP_JAR} server etc/${project.artifactId}/configuration.yaml
 else
     # compile and install projects before running
-    cd ${LIBDIR} && mvn -DskipTests=true compile install && mvn -DskipTests=true exec:java -pl ${project.artifactId}
+    cd ${LIBDIR} && mvn -DskipTests=true compile install && mvn -DskipTests=true exec:exec -pl ${project.artifactId}
 fi
